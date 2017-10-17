8192febd9b569767d8394acb0ae709f6e403cbb8
Ravinder <lravinder@ssdi.sharp.co.in>
2017-10-17 17:20:41 +0530

    Added Session Attributes

--- MfpCopies/src/copy.properties.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var copyProperties = {
-  "copies" : "1",
-  "side" : "two",
-  "colour" : "colour",
-  "staple" : "stapled"
-}
-module.exports.copyProperties = copyProperties;
--- MfpCopies/src/handlers/copies.handlers.js
+++ MfpCopies/src/handlers/copies.handlers.js
@@ -2,8 +2,6 @@
 
 const Alexa = require('alexa-sdk');
 const MFP_STATES = require('../enums').MFP_STATES;
-var copyproperties = require('../copy.properties').copyProperties;
-
 
 function removeSSML(s) {
   return s.replace(/<\/?[^>]+(>|$)/g, "");
@@ -11,36 +9,43 @@ function removeSSML(s) {
 
 module.exports = Alexa.CreateStateHandler(MFP_STATES.COPIESMODE, {
   'NewSession': function() {
+    var copy = {
+      "copies": "1",
+      "colour": "colour",
+      "side": "two",
+      "staple": "stapled"
+    };
+    this.attributes['copy'] = copy;
     this.emit('NewSession'); // Uses the handler in newSessionHandlers
   },
   'GetCopies': function() {
-    // Use this.t() to get corresponding language data
-    console.log('GetCopies ' + JSON.stringify(this.event, null, '\t'));
 
     // set state to asking questions
     this.handler.state = MFP_STATES.COPIESMODE;
+
+    console.log('GetCopies ' + JSON.stringify(this.event, null, '\t'));
     if (this.event.request.dialogState === "STARTED") {
 
       var updatedIntent = this.event.request.intent;
 
       if (this.event.request.intent.slots.side != null &&
         this.event.request.intent.slots.side.value == undefined) {
-        updatedIntent.slots.side.value = copyproperties.side;
+        updatedIntent.slots.side.value = this.event.session.attributes['copy'].side;
       }
 
       if (this.event.request.intent.slots.colour != null &&
         this.event.request.intent.slots.colour.value == undefined) {
-        updatedIntent.slots.colour.value = copyproperties.colour;
+        updatedIntent.slots.colour.value = this.event.session.attributes['copy'].colour;
       }
 
       if (this.event.request.intent.slots.staple != null &&
         this.event.request.intent.slots.staple.value == undefined) {
-        updatedIntent.slots.staple.value = copyproperties.staple;
+        updatedIntent.slots.staple.value = this.event.session.attributes['copy'].staple;
       }
 
       if (this.event.request.intent.slots.copies != null &&
         this.event.request.intent.slots.copies.value == undefined) {
-        updatedIntent.slots.copies.value = copyproperties.copies;
+        updatedIntent.slots.copies.value = this.event.session.attributes['copy'].copies;
       }
 
       this.emit(":delegate", updatedIntent);
@@ -48,20 +53,26 @@ module.exports = Alexa.CreateStateHandler(MFP_STATES.COPIESMODE, {
       this.emit(":delegate");
     } else if (this.event.request.intent.confirmationStatus === 'DENIED') {
       if (this.event.request.intent.slots.side != null &&
-        this.event.request.intent.slots.side.value == undefined) {
-        copyproperties.side = this.event.request.intent.slots.side.value.toLowerCase();
+        this.event.request.intent.slots.side.value != undefined) {
+        this.event.session.attributes['copy'].side = this.event.request.intent.slots.side.value.toLowerCase();
       }
 
       if (this.event.request.intent.slots.colour != null &&
-        this.event.request.intent.slots.colour.value == undefined) {
-        copyproperties.colour = this.event.request.intent.slots.colour.value.toLowerCase();
+        this.event.request.intent.slots.colour.value != undefined) {
+        this.event.session.attributes['copy'].colour = this.event.request.intent.slots.colour.value.toLowerCase();
       }
 
       if (this.event.request.intent.slots.staple != null &&
-        this.event.request.intent.slots.staple.value == undefined) {
-        copyproperties.staple = this.event.request.intent.slots.staple.value.toLowerCase();
+        this.event.request.intent.slots.staple.value != undefined) {
+        this.event.session.attributes['copy'].staple = this.event.request.intent.slots.staple.value.toLowerCase();
       }
 
+      if (this.event.request.intent.slots.copies != null &&
+        this.event.request.intent.slots.copies.value != undefined) {
+        this.event.session.attributes['copy'].copies = this.event.request.intent.slots.copies.value.toLowerCase();
+      }
+
+      console.log('GetCopies Deny' + JSON.stringify(this.event, null, '\t'));
       const slotToElicit = 'copies'
       const speechOutput = 'Which setting you wish to update?';
       const repromptSpeech = 'say set with property name'
@@ -72,10 +83,10 @@ module.exports = Alexa.CreateStateHandler(MFP_STATES.COPIESMODE, {
         let speechOutput = "Slots Completed";
         console.log('GetCopies slot completed ' + JSON.stringify(this.event, null, '\t'));
 
-        speechOutput = "Making " + copyproperties.side + "  sided ";
-        speechOutput += copyproperties.colour + " ";
-        speechOutput += copyproperties.copies + " copies ";
-        speechOutput += copyproperties.staple;
+        speechOutput = "Making " + this.event.request.intent.slots.side.value.toLowerCase() + "  sided ";
+        speechOutput += this.event.request.intent.slots.colour.value.toLowerCase() + " ";
+        speechOutput += this.event.request.intent.slots.copies.value.toLowerCase() + " copies ";
+        speechOutput += this.event.request.intent.slots.staple.value.toLowerCase();
 
         // Create speech output
         this.response.cardRenderer(this.t('SKILL_NAME'), removeSSML(speechOutput));
